315d0f8289093d639fac2be2cc02774ff3732739
Artur Ostrowski <artur.ostrowski@almestinio.pl>
2018-03-12 20:23:55 +0100

    Removed firebase from login & register

--- app/src/main/java/pl/almestinio/socialapp/ui/LoginView/LoginViewPresenter.java
+++ app/src/main/java/pl/almestinio/socialapp/ui/LoginView/LoginViewPresenter.java
@@ -1,14 +1,6 @@
 package pl.almestinio.socialapp.ui.loginView;
 
 import android.app.Activity;
-import android.support.annotation.NonNull;
-import android.util.Log;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 import pl.almestinio.socialapp.database.DatabaseUser;
 import pl.almestinio.socialapp.http.RestClient;
@@ -36,30 +28,30 @@ public class LoginViewPresenter implements LoginViewContracts.LoginViewPresenter
     @Override
     public void onLoginButtonClick(String username, String password) {
 
-        FirebaseAuth mAuth = FirebaseAuth.getInstance();
-
-        mAuth.signInWithEmailAndPassword(username, password)
-                .addOnCompleteListener(context, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d("xD", "signInWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            loginView.showToast("Authentication success.");
-
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w("xD", "signInWithEmail:failure", task.getException());
-//                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
-//                                    Toast.LENGTH_SHORT).show();
-                            loginView.showToast("Authentication failed.");
-//                            updateUI(null);
-                        }
-
-                        // ...
-                    }
-                });
+//        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+//
+//        mAuth.signInWithEmailAndPassword(username, password)
+//                .addOnCompleteListener(context, new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if (task.isSuccessful()) {
+//                            // Sign in success, update UI with the signed-in user's information
+//                            Log.d("xD", "signInWithEmail:success");
+//                            FirebaseUser user = mAuth.getCurrentUser();
+//                            loginView.showToast("Authentication success.");
+//
+//                        } else {
+//                            // If sign in fails, display a message to the user.
+//                            Log.w("xD", "signInWithEmail:failure", task.getException());
+////                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+////                                    Toast.LENGTH_SHORT).show();
+//                            loginView.showToast("Authentication failed.");
+////                            updateUI(null);
+//                        }
+//
+//                        // ...
+//                    }
+//                });
 
         loginView.showToast("Login Button Clicked!");
         try{
--- app/src/main/java/pl/almestinio/socialapp/ui/RegisterView/RegisterViewPresenter.java
+++ app/src/main/java/pl/almestinio/socialapp/ui/RegisterView/RegisterViewPresenter.java
@@ -1,14 +1,6 @@
 package pl.almestinio.socialapp.ui.registerView;
 
 import android.app.Activity;
-import android.support.annotation.NonNull;
-import android.util.Log;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 import pl.almestinio.socialapp.http.RestClient;
 import pl.almestinio.socialapp.http.user.User;
@@ -34,25 +26,23 @@ public class RegisterViewPresenter implements RegisterViewContracts.RegisterView
     @Override
     public void onRegisterButtonClick(String username, String fullName, String password) {
 
-        FirebaseAuth mAuth = FirebaseAuth.getInstance();
-
-        mAuth.createUserWithEmailAndPassword(username, password).addOnCompleteListener(context, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d("x", "createUserWithEmail:success");
-                    FirebaseUser user = mAuth.getCurrentUser();
-
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w("x", "createUserWithEmail:failure", task.getException());
-
-                }
-            }
-        });
-
-
+//        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+//
+//        mAuth.createUserWithEmailAndPassword(username, password).addOnCompleteListener(context, new OnCompleteListener<AuthResult>() {
+//            @Override
+//            public void onComplete(@NonNull Task<AuthResult> task) {
+//                if (task.isSuccessful()) {
+//                    // Sign in success, update UI with the signed-in user's information
+//                    Log.d("x", "createUserWithEmail:success");
+//                    FirebaseUser user = mAuth.getCurrentUser();
+//
+//                } else {
+//                    // If sign in fails, display a message to the user.
+//                    Log.w("x", "createUserWithEmail:failure", task.getException());
+//
+//                }
+//            }
+//        });
 
         if(fullName.length()>0 && username.length()>0 && password.length()>0){
             RestClient.getClient().requestUsers().enqueue(new Callback<Users>() {
